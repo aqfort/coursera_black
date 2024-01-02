18a294656e6c5a9e9265eceb92ae79f850764198
aqfort <aqfort.cpp@gmail.com>
2024-01-02 08:38:57 +0300

    Environment

--- initial.cpp
+++ initial.cpp
@@ -4,32 +4,32 @@
 
 void Test()
 {
-    ASSERT_EQUAL(3, Sum(1, 2));
+  ASSERT_EQUAL(3, Sum(1, 2));
 }
 
 int main(int argc, const char *const *const argv, const char *const *const envp)
 {
-    printf("argc = %d\n", argc);
+  printf("argc = %d\n", argc);
+  {
+    LOG_DURATION("Section: argv");
+    int i = 0;
+    char **pointer = const_cast<char **>(argv);
+    while (*pointer)
     {
-        LOG_DURATION("Section: argv");
-        int i = 0;
-        char **pointer = const_cast<char **>(argv);
-        while (*pointer)
-        {
-            printf("argv[%d] = %s\n", i++, *pointer++);
-        }
+      printf("argv[%d] = %s\n", i++, *pointer++);
     }
-    {
-        LOG_DURATION("Section: envp");
-        int i = 0;
-        char **pointer = const_cast<char **>(envp);
-        while (*pointer)
-            printf("envp[%d] = %s\n", i++, *pointer++);
-    }
-    {
-        LOG_DURATION();
-        TestRunner test_runner;
-        RUN_TEST(test_runner, Test);
-    }
-    return 0;
+  }
+  {
+    LOG_DURATION("Section: envp");
+    int i = 0;
+    char **pointer = const_cast<char **>(envp);
+    while (*pointer)
+      printf("envp[%d] = %s\n", i++, *pointer++);
+  }
+  {
+    LOG_DURATION();
+    TestRunner test_runner;
+    RUN_TEST(test_runner, Test);
+  }
+  return 0;
 }
